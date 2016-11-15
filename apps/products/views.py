4f9b99ef5f8379d4214c1e7489714d7a78b73c07
andycarver <andrewmcarver@gmail.com>
2016-11-14 20:21:36 -0800

    added the methods needed for the site

--- apps/products/views.py
+++ apps/products/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render, HttpResponse
   # Create your views here.
 def index(request):
     return render(request, "products/index.html")
+
+def show(request):
+    pass
+
+def new(request):
+    pass
+
+def edit(request):
+    pass
+
+def create(request):
+    pass
+
+def update(request):
+    pass
+
+def destroy(request):
+    pass
