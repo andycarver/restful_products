84c815b5cae0558ed6016058b2f47a2f5ee0dc71
andycarver <andrewmcarver@gmail.com>
2016-11-14 20:29:56 -0800

    started models

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    price = models.
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
