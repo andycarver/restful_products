917c8373e013de6f30f1d4282950a25be242c159
andycarver <andrewmcarver@gmail.com>
2016-11-15 07:14:04 -0800

    added price datafield

--- apps/products/models.py
+++ apps/products/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length = 50)
     description = models.CharField(max_length = 100)
-    price = models.
+    price = models.DecimalField(max_digits=19, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
